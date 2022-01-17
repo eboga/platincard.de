0e8fd83a676b708b94d0b9f9b3a86ab70dc11df5
boga <erkan.boga@qmedia.de>
2022-01-17 13:39:49 +0800

    Update 2022-01-17_13:39:49

--- deploy.sh
+++ deploy.sh
@@ -4,12 +4,12 @@ echo "* Build site"
 jekyll build > /dev/null
 
 echo "* Clean deployment dir"
-cd ../www.platincard.de
+cd /home/boga/projects/z-builds/www.platincard.de/
 shopt -u dotglob
 rm -rf *
 
 echo "* Copy files"
-cp -R ../x-platincard.de/_site/* . > /dev/null
+cp -R /home/boga/projects/x-platincard.de/_site/* . > /dev/null
 
 echo "* Add untracked files"
 git add .
@@ -18,4 +18,4 @@ echo "* Commit changes"
 git commit -m "Update $(date +%Y-%m-%d_%H:%M:%S)" > /dev/null
 
 echo "* Publish"
-git push
+git push origin master
